8ec1b994a457320f9576c9b8b638cbbfecbbfa80
James-Sundby <119804969+James-Sundby@users.noreply.github.com>
2023-11-17 06:29:48 -0700

    Adds header to ChuckNorrisFact

--- Incredible_Todo_List_App/src/components/ChuckNorrisFact.jsx
+++ Incredible_Todo_List_App/src/components/ChuckNorrisFact.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Text, Button} from 'react-native';
+import {Text, Button, StyleSheet} from 'react-native';
 //Click inside the emulator and press Ctrl + M to open the developer menu. Click on "Debug JS Remotely" to open the Chrome Developer Tools. Click on the "Console" tab to see the console output from the app.
 
 const ChuckNorrisFact = ({}) => {
@@ -25,18 +25,26 @@ const ChuckNorrisFact = ({}) => {
 
   return (
     <>
-      <Text
-        style={{
-          fontSize: 22,
-          textAlign: 'center',
-          marginTop: 10,
-          marginBottom: 10,
-        }}>
-        {fact}
-      </Text>
+      <Text style={styles.heading}>Random Chuck Norris Fact</Text>
+      <Text style={styles.quote}>{fact}</Text>
       <Button title="Refresh" onPress={handleRefreshPress} />
     </>
   );
 };
+
+const styles = StyleSheet.create({
+  quote: {
+    fontSize: 20,
+    textAlign: 'center',
+    marginBottom: 10,
+  },
+  heading: {
+    fontSize: 24,
+    textAlign: 'center',
+    marginTop: 20,
+    marginBottom: 10,
+    fontWeight: 'bold',
+  },
+});
 
 export default ChuckNorrisFact;
